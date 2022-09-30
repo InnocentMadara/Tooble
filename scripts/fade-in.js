1f493c36cc99d957c3ea0cf119b237188ece6db7
Bohdan <95318127+InnocentMadara@users.noreply.github.com>
2022-09-30 17:26:53 +0300

    Delete scripts directory

--- scripts/fade-in.js
+++
@@ -1,5 +0,0 @@
-const fadeBlock = document.querySelector('.fade-in');
-
-window.addEventListener('load', function (e) {
-  fadeBlock.setAttribute("active", "");
-});
--- scripts/menu.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.discount-menu');
-const button = document.querySelector('.discount-menu__open-button');
-
-button.addEventListener("click", e => {
-  menu.classList.toggle("discount-menu--active")
-})
