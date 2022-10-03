4584acd7718567b377d8dd98eb052bd209fbc2b2
Bohdan <bogdankurilo76@gmail.com>
2022-10-03 05:13:53 +0300

    fixed videos + added "get-discount" block

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,11 @@
 const menu = document.querySelector('.discount-menu');
 const button = document.querySelector('.discount-menu__open-button');
+const close = document.querySelector('.discount-menu__close-button');
 
 button.addEventListener("click", e => {
-  menu.classList.toggle("discount-menu--active")
+  menu.classList.add("discount-menu--active")
+})
+
+close.addEventListener("click", e => {
+  menu.classList.remove("discount-menu--active")
 })
