9209a0b3a9691e77cf7c906a26d3062a78705a39
Bohdan <bogdankurilo76@gmail.com>
2022-12-04 16:52:56 +0200

    kjahldsjklfa

--- scripts/lines.js
+++ scripts/lines.js
@@ -7,8 +7,7 @@ let offset = items.map( item => item.clientWidth - Math.max(...(items.map(item=>
 let lineWidth = 0;
 
 let heightPercentage = [0.4, 0.1, 0.018];
-let height;
-// let height = [400, 105, 20];
+let lineHeight;
 
 let lineStart = 0;
 
@@ -16,23 +15,45 @@ let lineEndRight = 0;
 let lineEndTop = [0, 0, 0]; 
 
 window.addEventListener('load', function (e) {
-  height = heightPercentage.map( h => h * window.innerHeight );
+  
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  
+  let aspectRatio = width / height;
+  if(aspectRatio > 16/9)
+    aspectRatio = width / height / 16 * 9;
+  else
+    aspectRatio = 1;
+
+  lineHeight = heightPercentage.map( h => h * window.innerHeight * aspectRatio );
+
   lines.forEach((line, i) => {
-    line.style = `margin-left: ${60 - offset[i]}px; width: ${lineWidth}px; height: ${height[i]}px`;
+    line.style = `margin-left: ${60 - offset[i]}px; width: ${lineWidth}px; height: ${lineHeight[i]}px`;
   })
   renderLines();
 });
 
 
 window.addEventListener('resize', function (e) {
-  height = heightPercentage.map( h => h * window.innerHeight );
+  
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  
+  let aspectRatio = width / height;
+  if(aspectRatio > 16/9)
+    aspectRatio = width / height / 16 * 9;
+  else
+    aspectRatio = 1;
+    
+  lineHeight = heightPercentage.map( h => h * window.innerHeight * aspectRatio);
+
   renderLines();
 });
 
 
 function setStyles() {
   lines.forEach((line, i) => {
-    line.style = `margin-left: ${60 - offset[i]}px; width: ${lineWidth}px; height: ${height[i]}px`;
+    line.style = `margin-left: ${60 - offset[i]}px; width: ${lineWidth}px; height: ${lineHeight[i]}px`;
     line.querySelector('.line-right').style = `flex-basis: ${lineWidth * 0.3 + 30 - 30 * i}px`;
   })
 }
